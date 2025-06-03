01f749e993c77a4375e57f6e9325fe348263310e
lm7-1 <larryamercier@gmail.com>
2025-06-03 06:28:15 -0400

    commit

--- Quickstart-master/TeamCode/src/main/java/robot/PIDFTuning.java
+++ Quickstart-master/TeamCode/src/main/java/robot/PIDFTuning.java
@@ -4,16 +4,16 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class PIDFTuning {
-    public static double arm_kP = 0.005;
-    public static double arm_kI = 0.000;
-    public static double arm_kD = 0.00;
-    public static double arm_kF = 0.0001;
-    public static double armTargetVelocity = 1000.0;
+    public static double arm_kP = 0.01;
+    public static double arm_kI = 0.0;
+    public static double arm_kD = 0.001;
+    public static double arm_kF = 0.01;
+    public static double armTargetVelocity = 300.0;
 
-    public static double vslide_kP = 0.005;
-    public static double vslide_kI = 0.0002;
-    public static double vslide_kD = 0.0;
-    public static double vslide_kF = 0.0001;
-    public static double vslideTargetVelocity = 600.0;
+    public static double vslide_kP = 0.012;
+    public static double vslide_kI = 0.0;
+    public static double vslide_kD = 0.001;
+    public static double vslide_kF = 0.012;
+    public static double vslideTargetVelocity = 400.0;
 }
 
